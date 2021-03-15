5d2e1ddd493c1ffdbde9be43d4487f90a5ac12fc
Georgi Surchev <georgi.surchev@scalefocus.com>
2021-03-15 11:12:40 +0200

    -add setTextOrGone in BindingAdapter.kt

--- app/src/main/java/com/example/customview/cv/utils/BindingAdapter.kt
+++ app/src/main/java/com/example/customview/cv/utils/BindingAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.customview.cv.utils
 
+import android.view.View
 import android.widget.AutoCompleteTextView
+import android.widget.TextView
 import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
@@ -13,5 +15,16 @@ object BindingAdapter {
 	fun color(view: AutoCompleteTextView, @ColorInt color: Int) {
 		val inputColor = if (color == 0)  ContextCompat.getColor(view.context, R.color.black) else color
 		view.setTextColor(inputColor)
+	}
+
+	/**
+	 * Set content text to a text view, or hide the [TextView] if no text is provided
+	 */
+	@JvmStatic
+	@BindingAdapter("textOrGone")
+	fun setTextOrGone(textView: TextView, contentText: CharSequence?) {
+		val hideView = contentText.isNullOrBlank()
+		textView.visibility = if (hideView) View.GONE else View.VISIBLE
+		textView.text = contentText
 	}
 }
